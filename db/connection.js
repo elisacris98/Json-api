94aa03baeeda9e82f99f1ce368d3252de788fdfe
elisacris98 <cristina.elisa98@gmail.com>
2023-10-31 10:24:53 -0400

    added connections js

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,15 @@ import chalk from "chalk";
 
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/myhero", {
-            
-        })
-    }
-}
+        await mongoose.connect("mongodb://localhost:27017/project2", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(chalk.blue("Connected to MongoDB"));
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
